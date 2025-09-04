2d46b07a1c5a44bbb0778236ee46038bdea34223
marvinrobot78 <54140555+marvinrobot78@users.noreply.github.com>
2025-09-04 20:03:39 +1000

    Update light.py

--- components/pool_lights/light.py
+++ components/pool_lights/light.py
@@ -1,7 +1,9 @@
+from esphome import pins
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light, output
-from esphome.const import CONF_OUTPUT_ID
+from esphome.const import CONF_OUTPUT_ID, CONF_PIN
+from esphome.cpp_helpers import gpio_pin_expression
 
 rgb_ns = cg.esphome_ns.namespace("pool_lights")
 RGBLightOutput = rgb_ns.class_("PoolLights", light.LightOutput)
@@ -9,6 +11,7 @@ RGBLightOutput = rgb_ns.class_("PoolLights", light.LightOutput)
 CONFIG_SCHEMA = light.RGB_LIGHT_SCHEMA.extend(
     {
         cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(RGBLightOutput),
+        cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
     }
 )
 
@@ -17,3 +20,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     await light.register_light(var, config)
 
+    pin = await gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_output_pin(pin))
